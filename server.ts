557868cd0893e9ba49e69fc376bafb3e25c36869
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-10-30 23:52:41 +0700

    fix : port

--- server.ts
+++ server.ts
@@ -1,21 +1,24 @@
 import * as http from "http";
 import { Server } from "socket.io";
 import { CONFIG } from "./config/config";
-import { Coordinates } from "./types/type";
 import { createApp } from "./app";
 import { registerSocketEvents } from "./socket";
 
-
 const app = createApp();
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
     origin: CONFIG.CLIENT_ORIGIN,
     credentials: true,
   },
 });
+
 registerSocketEvents(io);
-server.listen(CONFIG.PORT, () => {
-  console.log(`Server running at ${CONFIG.PORT}`);
+
+const PORT = parseInt(process.env.PORT || String(CONFIG.PORT) || "3000", 10);
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
-// npx ts-node server.ts
+
